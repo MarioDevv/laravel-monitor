b1a014a531f05ade89ca08e7d246fe38a67db131
MarioDevv <mario@atsys.es>
2025-02-17 08:46:03 +0000

    Fix -> Order by & Command Config

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('ping:monitors')->everyFifteenMinutes();
+Schedule::command('ping:monitors')->everyThirtyMinutes();
